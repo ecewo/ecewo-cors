001279260dddd816a2608169f5a40d512101ac19
Savas <savashn@proton.me>
2026-01-11 20:39:03 +0300

    format

--- tests/test-custom-cors.c
+++ tests/test-custom-cors.c
@@ -5,81 +5,81 @@
 #include <string.h>
 
 int test_cors_custom_allowed_origin(void) {
-    MockHeaders headers[] = {
-        { "Origin", "http://localhost:3000" }
-    };
+  MockHeaders headers[] = {
+    { "Origin", "http://localhost:3000" }
+  };
 
-    MockParams params = {
-        .method = MOCK_GET,
-        .path = "/api/data",
-        .body = NULL,
-        .headers = headers,
-        .header_count = 1
-    };
+  MockParams params = {
+    .method = MOCK_GET,
+    .path = "/api/data",
+    .body = NULL,
+    .headers = headers,
+    .header_count = 1
+  };
 
-    MockResponse res = request(&params);
+  MockResponse res = request(&params);
 
-    ASSERT_EQ(200, res.status_code);
-    ASSERT_EQ_STR("CORS OK", res.body);
-    ASSERT_EQ_STR("http://localhost:3000", mock_get_header(&res, "Access-Control-Allow-Origin"));
-    ASSERT_EQ_STR("true", mock_get_header(&res, "Access-Control-Allow-Credentials"));
-    ASSERT_EQ_STR("X-Custom-Header", mock_get_header(&res, "Access-Control-Expose-Headers"));
+  ASSERT_EQ(200, res.status_code);
+  ASSERT_EQ_STR("CORS OK", res.body);
+  ASSERT_EQ_STR("http://localhost:3000", mock_get_header(&res, "Access-Control-Allow-Origin"));
+  ASSERT_EQ_STR("true", mock_get_header(&res, "Access-Control-Allow-Credentials"));
+  ASSERT_EQ_STR("X-Custom-Header", mock_get_header(&res, "Access-Control-Expose-Headers"));
 
-    free_request(&res);
-    RETURN_OK();
+  free_request(&res);
+  RETURN_OK();
 }
 
 int test_cors_custom_disallowed_origin(void) {
-    MockHeaders headers[] = {
-        { "Origin", "http://notallowed.com" }
-    };
+  MockHeaders headers[] = {
+    { "Origin", "http://notallowed.com" }
+  };
 
-    MockParams params = {
-        .method = MOCK_GET,
-        .path = "/api/data",
-        .body = NULL,
-        .headers = headers,
-        .header_count = 1
-    };
+  MockParams params = {
+    .method = MOCK_GET,
+    .path = "/api/data",
+    .body = NULL,
+    .headers = headers,
+    .header_count = 1
+  };
 
-    MockResponse res = request(&params);
+  MockResponse res = request(&params);
 
-    ASSERT_EQ(200, res.status_code);
-    ASSERT_NULL(mock_get_header(&res, "Access-Control-Allow-Origin"));
+  ASSERT_EQ(200, res.status_code);
+  ASSERT_NULL(mock_get_header(&res, "Access-Control-Allow-Origin"));
 
-    free_request(&res);
-    RETURN_OK();
+  free_request(&res);
+  RETURN_OK();
 }
 
 int test_cors_custom_preflight(void) {
-    MockHeaders headers[] = {
-        { "Origin", "http://example.com" },
-        { "Access-Control-Request-Method", "POST" },
-        { "Access-Control-Request-Headers", "Content-Type" }
-    };
+  MockHeaders headers[] = {
+    { "Origin", "http://example.com" },
+    { "Access-Control-Request-Method", "POST" },
+    { "Access-Control-Request-Headers", "Content-Type" }
+  };
 
-    MockParams params = {
-        .method = MOCK_OPTIONS,
-        .path = "/api/data",
-        .body = NULL,
-        .headers = headers,
-        .header_count = 3
-    };
+  MockParams params = {
+    .method = MOCK_OPTIONS,
+    .path = "/api/data",
+    .body = NULL,
+    .headers = headers,
+    .header_count = 3
+  };
 
-    MockResponse res = request(&params);
+  MockResponse res = request(&params);
 
-    ASSERT_EQ(204, res.status_code);
-    ASSERT_EQ_STR("http://example.com", mock_get_header(&res, "Access-Control-Allow-Origin"));
-    ASSERT_EQ_STR("true", mock_get_header(&res, "Access-Control-Allow-Credentials"));
-    ASSERT_EQ_STR("Content-Type, Authorization", mock_get_header(&res, "Access-Control-Allow-Headers"));
-    ASSERT_EQ_STR("GET, POST", mock_get_header(&res, "Access-Control-Allow-Methods"));
+  ASSERT_EQ(204, res.status_code);
+  ASSERT_EQ_STR("http://example.com", mock_get_header(&res, "Access-Control-Allow-Origin"));
+  ASSERT_EQ_STR("true", mock_get_header(&res, "Access-Control-Allow-Credentials"));
+  ASSERT_EQ_STR("Content-Type, Authorization", mock_get_header(&res, "Access-Control-Allow-Headers"));
+  ASSERT_EQ_STR("GET, POST", mock_get_header(&res, "Access-Control-Allow-Methods"));
 
-    free_request(&res);
-    RETURN_OK();
+  free_request(&res);
+  RETURN_OK();
 }
 
 void handler_cors_test(Req *req, Res *res) {
-    send_text(res, 200, "CORS OK");
+  send_text(res, 200, "CORS OK");
 }
 
 void setup_all_routes(void) {
@@ -101,7 +101,6 @@ static const Cors custom_cors = {
   .max_age = 600
 };
 
-
 int main(void) {
   if (mock_init(setup_all_routes) != 0) {
     printf("ERROR: Failed to initialize mock server\n");
@@ -113,7 +112,7 @@ int main(void) {
     mock_cleanup();
     return 1;
   }
-  
+
   RUN_TEST(test_cors_custom_allowed_origin);
   RUN_TEST(test_cors_custom_disallowed_origin);
   RUN_TEST(test_cors_custom_preflight);
--- tests/test-default-cors.c
+++ tests/test-default-cors.c
@@ -5,68 +5,68 @@
 #include <string.h>
 
 void handler_cors_test(Req *req, Res *res) {
-    send_text(res, 200, "CORS OK");
+  send_text(res, 200, "CORS OK");
 }
 
 int test_cors_preflight_request(void) {
-    MockHeaders headers[] = {
-        { "Origin", "http://localhost:3000" },
-        { "Access-Control-Request-Method", "POST" }
-    };
+  MockHeaders headers[] = {
+    { "Origin", "http://localhost:3000" },
+    { "Access-Control-Request-Method", "POST" }
+  };
 
-    MockParams params = {
-        .method = MOCK_OPTIONS,
-        .path = "/api/data",
-        .body = NULL,
-        .headers = headers,
-        .header_count = 2
-    };
+  MockParams params = {
+    .method = MOCK_OPTIONS,
+    .path = "/api/data",
+    .body = NULL,
+    .headers = headers,
+    .header_count = 2
+  };
 
-    MockResponse res = request(&params);
+  MockResponse res = request(&params);
 
-    ASSERT_EQ(204, res.status_code);
+  ASSERT_EQ(204, res.status_code);
 
-    free_request(&res);
-    RETURN_OK();
+  free_request(&res);
+  RETURN_OK();
 }
 
 int test_cors_simple_request(void) {
-    MockHeaders headers[] = {
-        { "Origin", "http://localhost:3000" }
-    };
+  MockHeaders headers[] = {
+    { "Origin", "http://localhost:3000" }
+  };
 
-    MockParams params = {
-        .method = MOCK_GET,
-        .path = "/api/data",
-        .body = NULL,
-        .headers = headers,
-        .header_count = 1
-    };
+  MockParams params = {
+    .method = MOCK_GET,
+    .path = "/api/data",
+    .body = NULL,
+    .headers = headers,
+    .header_count = 1
+  };
 
-    MockResponse res = request(&params);
+  MockResponse res = request(&params);
 
-    ASSERT_EQ(200, res.status_code);
-    ASSERT_EQ_STR("CORS OK", res.body);
+  ASSERT_EQ(200, res.status_code);
+  ASSERT_EQ_STR("CORS OK", res.body);
 
-    free_request(&res);
-    RETURN_OK();
+  free_request(&res);
+  RETURN_OK();
 }
 
 int test_cors_no_origin(void) {
-    MockParams params = {
-        .method = MOCK_GET,
-        .path = "/api/data",
-        .body = NULL,
-        .headers = NULL,
-        .header_count = 0
-    };
+  MockParams params = {
+    .method = MOCK_GET,
+    .path = "/api/data",
+    .body = NULL,
+    .headers = NULL,
+    .header_count = 0
+  };
 
-    MockResponse res = request(&params);
+  MockResponse res = request(&params);
 
-    ASSERT_EQ(200, res.status_code);
+  ASSERT_EQ(200, res.status_code);
 
-    free_request(&res);
-    RETURN_OK();
+  free_request(&res);
+  RETURN_OK();
 }
 
 void setup_all_routes(void) {
